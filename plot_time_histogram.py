c8cd1d0b050ddcb5b927a0bed17780538fd938e0
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2013-04-12 20:20:05 -0300

    Fixes plot script to new filename standard

--- plot_time_histogram.py
+++ plot_time_histogram.py
@@ -7,12 +7,12 @@ from matplotlib import pyplot
 
 # There are some outliers that make the histogram useless. For now we'll remove
 # any execution time above 1 second.
-THRESHOLD = 1.0
+THRESHOLD = 30.0
 
 data = {}
 outliers = {}
 
-filenames = glob.glob("data/worker-*.csv")
+filenames = glob.glob("data/worker-*.dat")
 if not filenames:
     sys.stderr.write("You need data files in data/worker-*.dat\n")
     sys.exit(1)
@@ -33,7 +33,7 @@ for filename in filenames:
 
 labels = ["{} ({})".format(worker, outliers[worker]) for worker in data.keys()]
 pyplot.hist(data.values(), label=labels)
-pyplot.legend(title="Worker (outliers removed)", fontsize="small",
+pyplot.legend(title="Worker (outliers removed)", #fontsize="small",
         fancybox=True)
 pyplot.suptitle("Execution times greater than {}s were "
         "removed".format(THRESHOLD))
